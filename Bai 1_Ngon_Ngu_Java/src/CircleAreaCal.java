6a974a07974487fa5b8c803c7e86015572ecdc80
viet <thanhvietbkdn@gmail.com>
2023-07-05 14:39:07 +0700

    dua bai len

--- Bai 1_Ngon_Ngu_Java/src/CircleAreaCal.java
+++
@@ -1,8 +0,0 @@
-public class CircleAreaCal {
-    public static void main(String[] args) {
-        double radius=50.0;
-        final double PI=3.14159;
-        double area=PI*radius*radius;
-        System.out.println("Diện tích hình tròn là: "+area);
-    }
-}
